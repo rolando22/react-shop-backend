0c12fbc81e6111219ea52bdfbc88df2862861a82
rolando22 <rolando2207@gmail.com>
2022-11-18 15:36:48 -0300

    [ADD] nuevas rutas

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/route-new', (req, res) => {
+    res.send('Hi, I am new route');
+});
+
+app.get('/product', (req, res) => {
+    res.json({
+        name: 'Product 1',
+        price: 1000,
+    });
+});
+
 app.listen(port, () => {
     console.log(`Listening at http://localhost: ${port}`);
 });
