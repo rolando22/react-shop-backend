8751694233edb97456b09a095fbed0e015f03668
rolando22 <rolando2207@gmail.com>
2022-11-18 15:31:07 -0300

    [ADD] Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('My App');
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost: ${port}`);
+});
