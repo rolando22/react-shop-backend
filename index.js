de0b33dc3087207447569903c25e6b160a36bfa0
rolando22 <rolando2207@gmail.com>
2022-11-18 23:58:41 -0300

    [ADD] nuevas rutas con parámetros

--- index.js
+++ index.js
@@ -7,14 +7,33 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/route-new', (req, res) => {
-    res.send('Hi, I am new route');
+app.get('/products', (req, res) => {
+    res.json([
+        {
+            name: 'Product 1',
+            price: 1000,
+        },
+        {
+            name: 'Product 2',
+            price: 2000,
+        }
+    ]);
 });
 
-app.get('/product', (req, res) => {
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params;
     res.json({
+        id: id,
         name: 'Product 1',
         price: 1000,
+    });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+    const { categoryId, productId } = req.params;
+    res.json({
+        categoryId: categoryId,
+        productId: productId,
     });
 });
 
