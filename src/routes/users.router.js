55870ab3ca5f03cdc08bba98223ad0eb0823b10f
rolando22 <rolando2207@gmail.com>
2022-12-15 21:36:54 -0300

    [ADD] CRUD User, router, service y schema

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,16 +1,79 @@
 const router = require('express').Router();
 
 const UserService = require('../services/user.service');
+const { validatorHandler } = require('../middlewares/validator.handler');
+const { 
+    createUserSchema, 
+    updatedUserSchema, 
+    getUserSchema 
+} = require('../schemas/user.schema');
 
 const service = new UserService;
 
 router.get('/', async (req, res, next) => {
     try {
         const users = await service.find();
-        res.status(200).json(users);
+
+        res.status(200).json({ data: users });
     } catch (error) {
         next(error);
     }
 });
+
+router.get('/:id', 
+    validatorHandler(getUserSchema, 'params'), 
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const user = await service.findOne(id);
+            
+            res.status(200).json({ data: user });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.post('/', 
+    validatorHandler(createUserSchema, 'body'), 
+    async (req, res, next) => {
+        try {
+            const newUser = await service.create(req.body);
+
+            res.status(201).json({ message: 'created', data: newUser });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.patch('/:id', 
+    validatorHandler(getUserSchema, 'params'), 
+    validatorHandler(updatedUserSchema, 'body'), 
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const updatedUser = await service.update(id, req.body);
+
+            res.status(200).json({ message: 'updated', data: updatedUser });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.delete('/:id', 
+    validatorHandler(getUserSchema, 'params'),
+    async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const deletedUserId = await service.delete(id);
+
+            res.status(201).json({ message: 'deleted', data: deletedUserId });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
 
 module.exports = router;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -9,6 +9,29 @@ class UserService {
         const users = await models.User.findAll();
         return users;
     }
+
+    async findOne (id) {
+        const user = await models.User.findByPk(id);
+        if (!user) throw boom.notFound('user not found');
+        return user;
+    }
+
+    async create (data) {
+        const newUser = await models.User.create(data);
+        return newUser;
+    }
+
+    async update (id, changes) {
+        const user = await this.findOne(id);
+        const updatedUser = await user.update(changes);
+        return updatedUser;
+    }
+
+    async delete (id) {
+        const user = await this.findOne(id);
+        await user.destroy();
+        return id;
+    }
 };
 
 module.exports = UserService;
