6fdf2f9a7234d0a4e6b533cb4363e833ebe0e577
rolando22 <rolando2207@gmail.com>
2022-12-16 23:15:45 -0300

    [ADD] middlewares ormErrorHandler

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const routerApi = require('./routes');
-const { logErrors, boomErrorHandler, errorHandler } = require('./middlewares/error.handler');
+const { logErrors, ormErrorHandler, boomErrorHandler, errorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -26,6 +26,7 @@ app.get('/', (req, res) => {
 routerApi(app);
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
--- src/middlewares/error.handler.js
+++ src/middlewares/error.handler.js
@@ -1,7 +1,20 @@
+const { ValidationError } = require('sequelize');
+
 function logErrors (err, req, res, next) {
     console.log(err);
     next(err);
-};
+}
+
+function ormErrorHandler (err, req, res, next) {
+    if (err instanceof ValidationError) {
+        return res.status(409).json({
+            statusCode: 409,
+            message: err.name,
+            errors: err.errors,
+        });
+    }
+    next(err);
+}
 
 function boomErrorHandler (err, req, res, next) {
     if (err.isBoom) {
@@ -9,13 +22,13 @@ function boomErrorHandler (err, req, res, next) {
         return res.status(output.statusCode).json(output.payload);
     }
     next(err);
-};
+}
 
 function errorHandler (err, req, res, next) {
     res.status(500).json({
         message: err.message,
         stack: err.stack,
     });
-};
+}
 
-module.exports = { logErrors, boomErrorHandler, errorHandler };
+module.exports = { logErrors, ormErrorHandler, boomErrorHandler, errorHandler };
